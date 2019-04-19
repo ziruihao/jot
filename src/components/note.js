f5f6fb9d84f44916a46f0a48393ebb829d1d14f1
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-04-18 23:29:56 -0400

    a git mishap :/

---
+++ src/components/note.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Draggable from 'react-draggable';
+
+class Note extends Component {
+  consturctor(props) {
+    super(props);
+    this.state = {
+      id: props.id,
+      title: props.title,
+      text: props.text,
+      x: props.x,
+      y: props.y,
+      z: props.z,
+    };
+  }
+
+  render() {
+    return (
+      <Draggable />
+    );
+  }
+}
+
+export default Note;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Map } from 'immutable';
 import './style.scss';
 
-const App = () => <div className="test">All the REACTS are belong to us!</div>;
+// importing components
+import Note from './components/note';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: new Map({ test: 'a' }),
+    };
+    this.test_AddNote = this.test_AddNote.bind(this);
+  }
+
+  testAddNote = (note) => {
+    this.setState(
+      prevState => ({
+        notes: prevState.notes.set('test', note),
+      }),
+    );
+  }
+
+  render() {
+    const newNote = new Note();
+    this.testAddNote(newNote);
+    console.log(this.state.notes);
+    return (
+      <div />
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('main'));
