c6d869a9394d4cd96cd66dd0e31d39f934b09cef
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-04-22 19:10:27 -0400

    implemented material-ui

--- src/components/note.js
+++ src/components/note.js
@@ -2,6 +2,40 @@ import React, { Component } from 'react';
 import Draggable from 'react-draggable';
 import marked from 'marked';
 
+// material-ui components
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import CheckIcon from '@material-ui/icons/Check';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+
+/**
+ * Custom styling overrides for material-ui
+ */
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  card: {
+    minWidth: 275,
+    boxShadow: '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)',
+  },
+  'card:hover': {
+    boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
 class Note extends Component {
   constructor(props) {
     super(props);
@@ -42,10 +76,20 @@ class Note extends Component {
   }
 
   /**
-   * Updates the props.text with whatever is in the <textarea> field.
+   * Updates the props.text with whatever is in the <TextField> field.
    * @param {} event
    */
-  onInputChange = (event) => {
+  onInputChangeTitle = (event) => {
+    this.props.updateNote(this.props.note.id, {
+      title: event.target.value,
+    });
+  }
+
+  /**
+   * Updates the props.text with whatever is in the <TextField> field.
+   * @param {} event
+   */
+  onInputChangeText = (event) => {
     this.props.updateNote(this.props.note.id, {
       text: event.target.value,
     });
@@ -56,19 +100,45 @@ class Note extends Component {
   }
 
   renderWhetherEditing() {
-    let editOrNot = '';
+    const { classes } = this.props;
+    // classes.card.width = 'box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)';
     if (this.state.isEditing) {
-      editOrNot = 'save';
       return (
-        <div className="note" style={{ left: this.props.note.x, top: this.props.note.y }}>
-          <p>{this.props.note.title}</p>
-          <i onClick={this.deleteNote} role="button" tabIndex={0} className="fa fa-trash-o" />
-          <button type="button" onClick={this.toggleEdit}>{editOrNot}</button>
-          <textarea id="note-editor" name="textarea" onChange={this.onInputChange} value={this.props.note.text} />
-        </div>
+        <Card className={`${classes.card} note`} style={{ left: this.props.note.x, top: this.props.note.y, zIndex: this.props.note.z }}>
+          <CardContent>
+            <TextField
+              margin="dense"
+              id="noteTitleEditor"
+              type="text"
+              variant="outlined"
+              fullWidth
+              onChange={this.onInputChangeTitle}
+              value={this.props.note.title}
+            />
+            <TextField
+              autofocus
+              margin="dense"
+              id="notTextEditor"
+              type="text"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows="5"
+              onChange={this.onInputChangeText}
+              value={this.props.note.text}
+            />
+          </CardContent>
+          <CardActions>
+            <IconButton onClick={this.toggleEdit} className={classes.button} aria-label="Check">
+              <CheckIcon />
+            </IconButton>
+            <IconButton onClick={this.deleteNote} className={classes.button} aria-label="Delete">
+              <DeleteIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
       );
     } else {
-      editOrNot = 'edit';
       return (
         <Draggable
           handle=".draggable"
@@ -81,13 +151,25 @@ class Note extends Component {
           onStart={this.startDrag}
           onDrag={this.onDrag}
         >
-          <div className="draggable note" style={{ zIndex: this.props.note.z }}>
-            <p>{this.props.note.title}</p>
-            <button type="submit" onClick={this.deleteNote}>delete</button>
-            <i onClick={this.deleted} role="button" tabIndex={0} className="fa fa-trash-o" />
-            <button type="button" onClick={this.toggleEdit}>{editOrNot}</button>
-            <div className="noteBody" dangerouslySetInnerHTML={{ __html: marked(this.props.note.text || '') }} />
-          </div>
+          <Card className={`${classes.card} draggable note`} style={{ zIndex: this.props.note.z }}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                {this.props.note.title}
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                date_stamp
+              </Typography>
+              <Typography className="noteBody" component="p" dangerouslySetInnerHTML={{ __html: marked(this.props.note.text || '') }} />
+            </CardContent>
+            <CardActions>
+              <IconButton onClick={this.toggleEdit} className={classes.button} aria-label="Edit">
+                <EditIcon />
+              </IconButton>
+              <IconButton onClick={this.deleteNote} className={classes.button} aria-label="Delete">
+                <DeleteIcon />
+              </IconButton>
+            </CardActions>
+          </Card>
         </Draggable>
       );
     }
@@ -102,4 +184,4 @@ class Note extends Component {
   }
 }
 
-export default Note;
+export default withStyles(styles)(Note);
--- src/components/noteMaker.js
+++ src/components/noteMaker.js
@@ -1,43 +1,150 @@
 import React, { Component } from 'react';
 
+// material-ui components
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+/**
+ * Custom styling overrides for material-ui
+ */
+const styles = theme => ({
+  fab: {
+    margin: theme.spacing.unit,
+    position: 'absolute',
+    bottom: theme.spacing.unit * 2,
+    right: theme.spacing.unit * 2,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+});
+
 class NoteMaker extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: '',
+      text: '',
+      dialogOpen: false,
     };
-    this.onInputChange = this.onInputChange.bind(this);
   }
 
   /**
    * Updates the NoteMaker state with whatever is in the <input> field.
    * @param {} event
    */
-  onInputChange(event) {
+  onInputChangeTitle = (event) => {
     this.setState({
       title: event.target.value,
     });
+    console.log(this.state.title);
+  }
+
+  /**
+   * Updates the NoteMaker state with whatever is in the <input> field.
+   * @param {} event
+   */
+  onInputChangeText = (event) => {
+    this.setState({
+      text: event.target.value,
+    });
+    console.log(this.state.text);
   }
 
   /**
    * Activates callback to indicate that the user wants to create a new note to App.
    */
   createNote = () => {
-    this.props.createNote(this.state.title);
+    this.closeDialog();
+    this.cleanDialog();
+    this.props.createNote(this.state.title, this.state.text);
   }
 
+  /**
+   * Opens the dialog to create a new note.
+   */
+  openDialog = () => {
+    this.setState({
+      dialogOpen: true,
+    });
+  };
+
+  /**
+   * Clears out any entries in the dialog.
+   */
+  cleanDialog = () => {
+    this.setState({
+      title: '',
+      text: '',
+    });
+  }
+
+  /**
+   * Closes the dialog to create a new note.
+   */
+  closeDialog = () => {
+    this.setState({
+      dialogOpen: false,
+    });
+  };
+
   render() {
+    const { classes } = this.props;
     return (
       <div>
+        <Dialog
+          open={this.state.dialogOpen}
+          onClose={this.closeDialog}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">New note</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="noteTitle"
+              label="Note title"
+              type="text"
+              fullWidth
+              onChange={this.onInputChangeTitle}
+              value={this.state.title}
+            />
+            <TextField
+              margin="dense"
+              id="noteText"
+              label="Note body"
+              type="text"
+              fullWidth
+              multiline
+              rows="5"
+              onChange={this.onInputChangeText}
+              value={this.state.text}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.closeDialog} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.createNote} color="primary">
+              Create Note
+            </Button>
+          </DialogActions>
+        </Dialog>
         <div>
-          <input onChange={this.onInputChange} value={this.state.title} placeholder="take a note..." />
-        </div>
-        <div>
-          <button type="button" onClick={this.createNote}>new note</button>
+          <Fab color="primary" aria-label="Add" className={classes.fab} variant="extended" onClick={this.openDialog}>
+            <AddIcon className="extendedIcon" />New Note
+          </Fab>
         </div>
       </div>
     );
   }
 }
 
-export default NoteMaker;
+export default withStyles(styles)(NoteMaker);
